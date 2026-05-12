f3f35ebe0908ceec2c5436cb5f6ec1211a8f9fd9
Denis Penchugin <maverad@yandex.ru>
2026-05-12 22:56:48 +0300

    fixed task_4

--- task_4.py
+++ task_4.py
@@ -1,20 +1,20 @@
 class EmployeeSalary:
         hourly_payment = 400
 
-        def __init__(self, name='', hours=None, rest_days=0, email=None):
+        def __init__(self, name=None, hours=None, rest_days=0, email=None):
             self.name = name
             self.hours = hours
             self.rest_days = rest_days
             self.email = email
 
         @classmethod
-        def get_hours(cls, name='', hours=None, rest_days=0, email=None):
+        def get_hours(cls, name=None, hours=None, rest_days=0, email=None):
             if hours is None:
                 hours = (7 - rest_days) * 8
             return cls(name, hours, rest_days, email)
         
         @classmethod
-        def get_email(cls, name='', hours=None, rest_days=0, email=None):
+        def get_email(cls, name=None, hours=None, rest_days=0, email=None):
             if email is None:
                 email = f'{name}@email.com'
             return cls.get_hours(name, hours, rest_days, email)
@@ -25,3 +25,6 @@ class EmployeeSalary:
 
         def salary(self) -> int:
             return self.hourly_payment * self.hours
+        
+x = EmployeeSalary.get_email()
+print(x.email)
